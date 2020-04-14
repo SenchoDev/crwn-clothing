c65244e7dba00961cfc71b0d0bede37817c48f75
Senadin <beganovicsenadin24@gmail.com>
2020-04-14 17:51:00 +0200

    implement code that pulls in our shop data from firestore and stores it into the shop reducer from our shop page. Also included our withSpinner HOC

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -1,6 +1,7 @@
 import firebase from "firebase/app"; // we need base import for firestore and auth
 import "firebase/firestore"; // database
 import "firebase/auth";
+import { selectCollections } from "../redux/shop/shop.selectors";
 
 const config = {
   apiKey: "AIzaSyBLHk47jhet3XF5jYM_kTgp9af8Z48PwTw",
@@ -10,36 +11,65 @@ const config = {
   storageBucket: "crwn-db-17575.appspot.com",
   messagingSenderId: "195304571289",
   appId: "1:195304571289:web:9fe3734e6f0dca48061308",
-  measurementId: "G-KPFGYNT8L2"
+  measurementId: "G-KPFGYNT8L2",
 };
 
 export const createUserProfileDocument = async (userAuth, additionalData) => {
-  if(!userAuth)return;
+  if (!userAuth) return;
 
-  const userRef = firestore.doc(`users/${userAuth.uid}`)
+  const userRef = firestore.doc(`users/${userAuth.uid}`);
 
   const snapShot = await userRef.get();
 
-  if(!snapShot.exists){
-    const {displayName, email} = userAuth;
+  if (!snapShot.exists) {
+    const { displayName, email } = userAuth;
     const createdAt = new Date();
 
-    try{
+    try {
       await userRef.set({
         displayName,
         email,
         createdAt,
-        ...additionalData
-      })
-
-    } catch(error) {
-      console.log('error creating user', error)
-
+        ...additionalData,
+      });
+    } catch (error) {
+      console.log("error creating user", error);
     }
   }
   return userRef;
+};
 
-} 
+export const addCollectionAndDocuments = async (collectionKey, objectsToAdd) => {
+  const collectionRef = firestore.collection(collectionKey);
+  console.log(collectionRef);
+
+  const batch = firestore.batch()
+  objectsToAdd.forEach(obj => {
+    const newDocRef = collectionRef.doc();
+    batch.set(newDocRef, obj);
+  })
+  
+  return await batch.commit()
+}
+
+export const convertCollectionsSnapshotToMap = (collections) => {
+  const transformedCollection = collections.docs.map(doc => {
+    const {title, items} = doc.data();
+
+    return {
+      routeName: encodeURI(title.toLowerCase()),
+      id: doc.id,
+      title,
+      items
+    }
+  });
+
+  return transformedCollection.reduce((acc, collection)=> {
+    acc[collection.title.toLowerCase()] = collection;
+    return acc;
+  }, {})
+}
+
 
 firebase.initializeApp(config);
 
@@ -47,7 +77,7 @@ export const auth = firebase.auth();
 export const firestore = firebase.firestore();
 
 const porvider = new firebase.auth.GoogleAuthProvider();
-porvider.setCustomParameters({prompt: 'select_account'});
+porvider.setCustomParameters({ prompt: "select_account" });
 
 export const signInWithGoogle = () => auth.signInWithPopup(porvider);
 
--- src/redux/shop/shop.selectors.js
+++ src/redux/shop/shop.selectors.js
@@ -8,12 +8,12 @@ export const selectCollections = createSelector(
 );
 
 export const selectColectionsForPreview = createSelector(
-    [selectCollections],
-    collections => Object.keys(collections).map(key => collections[key])
-)
+  [selectCollections],
+  (collections) => collections ? Object.keys(collections).map((key) => collections[key]) :  []
+);
 
 export const selectCollection = (collectionUrlParam) =>
   createSelector(
     [selectCollections],
-    (collections) => collections[collectionUrlParam]
+    (collections) => collections ? collections[collectionUrlParam] : null
   );
